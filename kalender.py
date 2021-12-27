96c9e2ac5ce1a91653fde1f41e1fb21aa75f2488
dedi gusriyanto <dedigusriyanto@gmail.com>
2021-12-27 17:28:33 +0700

    first commit

---
+++ kalender.py
@@ -0,0 +1,19 @@
+#membuat kalender sederhana dengan code python
+import calendar
+yy =int (input("masukan tahun:"))
+mm =int (input("masukan bulan:"))
+# yy =  2021
+# mm = 12
+print(calendar.month(yy,mm))
+
+# DIDALAM KODINGAN INI ANDA BISA MEMILIH TAHUN DAN BULAN YANG ANDA INGINKAN.
+#CONTOH INPUTAN 
+# masukan tahun:2021
+# masukan bulan:12
+#    December 2021    
+# Mo Tu We Th Fr Sa Su
+#        1  2  3  4  5
+#  6  7  8  9 10 11 12
+# 13 14 15 16 17 18 19
+# 20 21 22 23 24 25 26
+# 27 28 29 30 31  
